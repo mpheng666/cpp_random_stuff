f676a015e28ac8ab139248479fad2d3fff708959
mpheng666 <mpheng96@gmail.com>
2022-09-23 08:10:35 +0800

    Modify structure

--- ds-algo/SearchPath.cpp
+++ ds-algo/SearchPath.cpp
@@ -11,83 +11,93 @@
 #include <iostream>
 #include <vector>
 
-struct Point {
-    int x_;
-    int y_;
-    Point(int x, int y) : x_(x), y_(y) {}
+struct Cell {
+    int x = -1;
+    int y = -1;
+    Cell(){};
+    Cell(int x, int y) : x(x), y(y) {}
+    Cell(Cell const& cell) {
+        x = cell.x;
+        y = cell.y;
+    }
 };
 
-class Solution {
-    private:
-        std::vector<Point> path_result_;
+class SearchPath {
+private:
+    bool found_path_{false};
+    std::vector<Cell> path_result_;
 
-    public:
-        std::vector<Point> searchPath(std::vector<std::vector<int>>& map,
-                                      Point start,
-                                      Point end) {
-            // std::vector<Point> path_result;
+public:
+    std::vector<Cell> searchPath(std::vector<std::vector<int>>& map,
+                                 Cell start,
+                                 Cell end) {
 
-            // check if out of bound
-            if (start.x_ >= map.size() || start.x_ < 0 ||
-                start.y_ >= map[0].size() || start.y_ < 0 ||
-                end.x_ >= map.size() || end.x_ < 0 || end.y_ >= map[0].size() ||
-                end.y_ < 0) {
-                std::cout << "Start or end out of bound" << '\n';
-                return path_result_;
-            }
+        Cell cell = startDFS(map, start, end);
 
-            startDFS(map, start, end);
 
-            return path_result_;
+        return path_result_;
     }
 
-    void startDFS(std::vector<std::vector<int>>& map, Point start, Point end) {
-        if (start.x_ >= map.size() || start.x_ < 0 ||
-            start.y_ >= map[0].size() || start.y_ < 0 || end.x_ >= map.size() ||
-            end.x_ < 0 || end.y_ >= map[0].size() || end.y_ < 0) {
+    Cell startDFS(std::vector<std::vector<int>>& map, Cell start, Cell end) {
+        Cell invalid_cell_(-1, -1);
+        Cell error_cell(-1, -1);
+        Cell current_cell = start;
+        if (start.x >= map.size() || start.x < 0 || start.y >= map[0].size() ||
+            start.y < 0 || end.x >= map.size() || end.x < 0 ||
+            end.y >= map[0].size() || end.y < 0) {
             // std::cout << "Start or end out of bound" << '\n';
-            return;
+            current_cell = error_cell;
+            return current_cell;
         }
-        if(map[start.x_][start.y_] == 0)
-        {
-            // std::cout << "Obstacle at " << start.x_ << " " << start.y_ << "\n";
-            return;
+        if (map[start.x][start.y] == 0) {
+            // std::cout << "Obstacle at " << start.x << " " << start.y <<
+            // "\n";
+            current_cell = error_cell;
+            return current_cell;
         }
-        if (map[start.x_][start.y_] == 1) {
-            // std::cout << "Visited at " << start.x_ << " " << start.y_ << "\n";
-            return;
-        }
-
-
-        if (start.x_ == end.x_ && start.y_ == end.y_) {
-
-            std::cout << "Found path at: " << start.x_ << " " << start.y_ << "\n";
-            map[start.x_][start.y_] = 1;
-            return;
+        if (map[start.x][start.y] == 1) {
+            // std::cout << "Visited at " << start.x << " " << start.y <<
+            // "\n";
+            current_cell = error_cell;
+            return current_cell;
         }
 
-        if (map[start.x_][start.y_] == -1) {
-            std::cout << "Valid cell: " << start.x_ << " " << start.y_ << "\n";
-            map[start.x_][start.y_] = 1;
-            path_result_.push_back(start);
+        if (start.x == end.x && start.y == end.y) {
+            std::cout << "Found goal at: " << start.x << " " << start.y << "\n";
+            map[start.x][start.y] = 1;
+            found_path_ = true;
+            // if(found_path_)
+            // path_result_.push_back(start);
+            return current_cell;
         }
 
-        start.y_ += 1;
-        startDFS(map, start, end);
-        start.y_ += -1;
+        if (map[start.x][start.y] == -1) {
+            // std::cout << "Valid cell: " << start.x << " " << start.y << "\n";
+            map[start.x][start.y] = 1;
+        }
 
-        start.x_ += -1;
-        startDFS(map, start, end);
-        start.x_ += 1;
+        start.y += 1;
+        current_cell = startDFS(map, start, end);
+        start.y += -1;
 
-        start.y_ += -1;
-        startDFS(map, start, end);
-        start.y_ += 1;
+        start.x += -1;
+        current_cell = startDFS(map, start, end);
+        start.x += 1;
 
-        start.x_ += 1;
-        startDFS(map, start, end);
-        start.x_ += -1;
+        start.y += -1;
+        current_cell = startDFS(map, start, end);
+        start.y += 1;
 
+        start.x += 1;
+        current_cell = startDFS(map, start, end);
+        start.x += -1;
+
+        if (current_cell.x != invalid_cell_.x &&
+            current_cell.y != invalid_cell_.y && found_path_) {
+            path_result_.push_back(current_cell);
+        }
+
+        return current_cell;
     }
 };
 
@@ -97,10 +107,10 @@ int main() {
     int map_row = 3;
     int map_col = 3;
 
-    // Point start_pos(3, 1);
-    // Point goal_pos(4, 6);
-    Point start_pos(0, 0);
-    Point goal_pos(2, 2);
+    // Cell start_pos(3, 1);
+    // Cell goal_pos(4, 6);
+    Cell start_pos(0, 0);
+    Cell goal_pos(2, 2);
 
     // init an unknown map map_row x map_col with -1
     std::vector<std::vector<int>> map(map_row, std::vector<int>(map_col, -1));
@@ -112,19 +122,22 @@ int main() {
     // map[3][4] = 0;
     // map[4][3] = 0;
     // map[4][4] = 0;
-    map[1][1] = 0;
-    map[1][2] = 0;
+    // map[1][1] = 0;
+    // map[1][2] = 0;
+    map[1][0] = 0;
 
-    Solution solution;
-    std::vector<Point> path = solution.searchPath(
-            map, start_pos, goal_pos);
+    SearchPath search_obj;
+    std::vector<Cell> path = search_obj.searchPath(map, start_pos, goal_pos);
 
-    for(const auto& c : path)
-    {
-        std::cout << "Path: " << c.x_ << ", " << c.y_ << " ";
+    for (const auto& cell : path) {
+        // if (cell.x == goal_pos.x && cell.y == goal_pos.y) start_print = true;
+        // if (cell.x == invalid_cell.x && cell.y == invalid_cell.y ||
+        //     !start_print)
+        //     continue;
+        std::cout << "Cell: " << cell.x << ", " << cell.y << '\n';
     }
 
-std::cout << '\n';
+    std::cout << '\n';
 
     return 0;
 };
