69eb05976c5858801d4ec222ddd9b9438cd30045
mpheng666 <mpheng96@gmail.com>
2022-10-08 00:02:18 +0800

    Add unordered json preOrderTraversal

---
+++ json_parser/src/main.cpp
@@ -0,0 +1,36 @@
+#include "../single_include/nlohmann/json.hpp"
+#include <fstream>
+#include <iostream>
+
+using json = nlohmann::json;
+using ordered_json = nlohmann::ordered_json;
+
+void preOrderTraverse(ordered_json OJSData)
+{
+    if (OJSData.empty()) {
+        return;
+    }
+
+    for (auto it = OJSData.begin(), end = OJSData.end(); it != end; ++it) {
+        if (it.value().is_object()) {
+            std::cout << it.key() << "\n";
+            preOrderTraverse(it.value());
+        } else {
+            std::cout << it.key() << " : " << it.value() << "\n";
+        }
+    }
+}
+
+int main() {
+    std::ifstream f("../data/default_tune_param.json");
+    ordered_json data = ordered_json::parse(f);
+    std::string s = data.dump();
+
+    // for (const auto &d : data) {
+    //     std::cout << d << "\n";
+    // }
+
+    preOrderTraverse(data);
+
+    return 0;
+}
