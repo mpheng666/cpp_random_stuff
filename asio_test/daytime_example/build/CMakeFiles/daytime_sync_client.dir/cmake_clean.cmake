4d3f1d4a64bad7f131b2bfff951b3a57d5c32fd5
mpheng666 <mpheng96@gmail.com>
2022-12-01 07:53:50 +0800

    Add asio daytime

---
+++ asio_test/daytime_example/build/CMakeFiles/daytime_sync_client.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/daytime_sync_client.dir/sync_daytime_client.cpp.o"
+  "daytime_sync_client"
+  "daytime_sync_client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/daytime_sync_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ asio_test/daytime_example/build/CMakeFiles/daytime_sync_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/daytime_sync_server.dir/sync_daytime_server.cpp.o"
+  "daytime_sync_server"
+  "daytime_sync_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/daytime_sync_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
