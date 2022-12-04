1cd93ba00ca134621c15734ad9b624468310fdc1
mpheng666 <mpheng96@gmail.com>
2022-12-05 01:04:01 +0800

    Test blocking tcp server with data

--- asio_test/cpp11/src/tcp_echo_server_blocking.cpp
+++ asio_test/cpp11/src/tcp_echo_server_blocking.cpp
@@ -2,12 +2,21 @@
 #include <boost/asio.hpp>
 #include <cstdlib>
 #include <iostream>
+#include <string>
 #include <thread>
+#include <unordered_map>
 #include <utility>
 
 using boost::asio::ip::tcp;
 
 const int max_length = 1024;
+
+enum class RobotCommand : char { INIT = 'I', START = 'S', STOP = 'X' };
+
+std::unordered_map<std::string, RobotCommand> ROBOT_CMD_MAP{
+        {"INIT", RobotCommand::INIT},
+        {"START", RobotCommand::START},
+        {"STOP", RobotCommand::STOP}};
 
 void session(tcp::socket sock) {
     try {
@@ -21,7 +30,30 @@ void session(tcp::socket sock) {
             else if (error)
                 throw boost::system::system_error(error); // Some other error.
             if (length) {
-                std::cout << data << "\n";
+                std::string data_str(data, length);
+                std::cout << data_str << "\n";
+                if (ROBOT_CMD_MAP.find(data_str) != ROBOT_CMD_MAP.end()) {
+                    RobotCommand new_command = ROBOT_CMD_MAP.at(data_str);
+                    switch (new_command) {
+                    case RobotCommand::INIT: {
+                        std::cout << "INIT command \n";
+                        break;
+                    }
+                    case RobotCommand::START: {
+                        std::cout << "START command \n";
+                        break;
+                    }
+                    case RobotCommand::STOP: {
+                        std::cout << "STOP command \n";
+                        break;
+                    }
+                    default: {
+                        break;
+                    }
+                    }
+                } else {
+                    std::cout << "Invalid command! \n";
+                }
             }
 
             boost::asio::write(sock, boost::asio::buffer(data, length));
