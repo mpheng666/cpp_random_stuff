577e8cf7711495e09bfe20ea79688b6c77bd8711
mpheng666 <mpheng96@gmail.com>
2023-01-19 14:10:53 +0800

    Add beast websocket

---
+++ beast_websocket/src/websocket/server/sync/build/CMakeFiles/websocket-server-sync.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mpheng/workspace/dev_workspace/cpp_random_stuff/beast_websocket/src/websocket/server/sync/websocket_server_sync.cpp" "/home/mpheng/workspace/dev_workspace/cpp_random_stuff/beast_websocket/src/websocket/server/sync/build/CMakeFiles/websocket-server-sync.dir/websocket_server_sync.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ beast_websocket/src/websocket/server/sync/build/CMakeFiles/websocket-server-sync.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/websocket-server-sync.dir/websocket_server_sync.cpp.o"
+  "websocket-server-sync.pdb"
+  "websocket-server-sync"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/websocket-server-sync.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
